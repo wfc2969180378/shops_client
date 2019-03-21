f7736bb139418f17d468ba7d6287c34d06335931
wfc2969180378 <2969180378@qq.com>
2019-03-21 14:06:57 +0800

    init shops-client

---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="footer_guide">
+    <router-link
+      class="guide_item"
+      :class="{on: '/miste' === $route.path}"
+      @click="goTo('/msite')"
+      to="/"
+      tag="a"
+    >
+      <span class="item_icon">
+          <i class="iconfont icon-waimai"></i>
+      </span>
+      <span class="item_text">
+       外卖
+      </span>
+    </router-link>
+    <router-link
+      class="guide_item"
+      :class="{on: '/search' === $route.path}"
+      @click="goTo('/search')"
+      to="/search"
+      tag="a"
+    >
+      <span class="item_icon">
+          <i class="iconfont icon-search"></i>
+      </span>
+      <span class="item_text">
+         搜索
+      </span>
+    </router-link>
+    <router-link
+      class="guide_item"
+      :class="{on: '/order' === $route.path}"
+      @click="goTo('/order')"
+      to="/order"
+      tag="a"
+    >
+      <span class="item_icon">
+          <i class="iconfont icon-dingdan"></i>
+      </span>
+      <span class="item_text">
+        订单
+      </span>
+    </router-link>
+    <router-link
+      class="guide_item"
+      :class="{on: '/profile' === $route.path}"
+      @click="goTo('/profile')"
+      to="/profile"
+      tag="a"
+    >
+      <span class="item_icon">
+          <i class="iconfont icon-geren"></i>
+      </span>
+      <span class="item_text">
+        我的
+      </span>
+    </router-link>
+  </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+          goTo(path) {
+            this.$router.replace(path);
+          }
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixins.styl"
+  .footer_guide
+    top-border-1px(#e4e4e4)
+    display flex
+    z-index 100
+    width 100%
+    height 50px
+    background #fcfcfc
+    position fixed
+    bottom 0
+    left 0
+    right 0
+    .guide_item
+      display flex
+      flex 1
+      flex-direction column
+      align-items center
+      margin 5px
+      &.on
+        color green !important
+      span
+        font-size 12px
+        margin-top 2px
+        margin-bottom 2px
+
+
+
+</style>
---
+++ src/components/HeaderTop/HeaderTop.vue
@@ -0,0 +1,63 @@
+<template>
+    <header class="header">
+      <slot name="left"></slot>
+      <span class="header_title">
+        <span class="header_title_text ellipsis">
+         {{title}}
+        </span>
+      </span>
+      <slot name="right"></slot>
+    </header>
+</template>
+
+<script>
+    export default {
+      name: "HeaderTop",
+      props: {
+        title: String
+      },
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .header
+    width 100%
+    height 45px
+    background #02a774
+    position fixed
+    left 0
+    top 0
+    z-index 100
+    .header_search
+      position absolute
+      left 15px
+      top 50%
+      transform translateY(-50%)
+      width 10%
+      height 50%
+      .icon-sousuo
+        font-size 25px
+        color #fff
+    .header_title
+      position absolute
+      left 50%
+      top 50%
+      transform translate(-50%,-50%)
+      width 50%
+      text-align center
+      color #fff
+      .header_title_text
+        display block
+        font-size 20px
+        color #fff
+    .header_login
+      position absolute
+      right 15px
+      top 50%
+      transform translateY(-50%)
+      font-size 14px
+      color #fff
+      .header_login_text
+        color #fff
+
+</style>
